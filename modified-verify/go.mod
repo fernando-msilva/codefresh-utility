58d3de4e4aeb821979c26aaac3a6922f8e29065c
fernando-msilva <fernandogbi96@gmail.com>
2022-01-24 23:03:10 -0300

    First commit
    Signed-off-by: fernando-msilva <fernandogbi96@gmail.com>

---
+++ modified-verify/go.mod
@@ -0,0 +1,3 @@
+module modified
+
+go 1.16
---
+++ modified-verify/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+type FilesChangeds struct {
+	HeadCommit struct {
+		Modified []string `json:"modified"`
+	} `json:"head_commit"`
+}
+
+func compareFiles(changeds []string, fileName string) {
+	for _, value := range changeds {
+		if value == fileName {
+			os.Exit(0)
+		}
+	}
+
+	os.Exit(1)
+}
+
+func main() {
+	jsonFile, _ := os.Open("/codefresh/volume/event.json")
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var teste FilesChangeds
+
+	json.Unmarshal(byteValue, &teste)
+
+	result := teste.HeadCommit.Modified
+
+	compareFiles(result, os.Args[1])
+
+}
